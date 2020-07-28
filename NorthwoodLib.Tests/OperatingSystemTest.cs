64cedf34667fad3de17c32cf794a6049c22c7f6a
petris <paprikapolishgamer@gmail.com>
2020-07-28 18:54:07 +0200

    Rework OperatingSystem

--- NorthwoodLib.Tests/OperatingSystemTest.cs
+++ NorthwoodLib.Tests/OperatingSystemTest.cs
@@ -1,20 +1,36 @@
 using System;
+using System.Runtime.InteropServices;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace NorthwoodLib.Tests
 {
 	public class OperatingSystemTest
 	{
+		private readonly ITestOutputHelper _output;
+		public OperatingSystemTest(ITestOutputHelper output) => _output = output;
+
+		[Fact]
+		public void UsesNativeDataTest()
+		{
+			Assert.Equal(RuntimeInformation.IsOSPlatform(OSPlatform.Windows), OperatingSystem.UsesNativeData);
+		}
+
 		[Fact]
 		public void CorrectStringTest()
 		{
-			Assert.Equal(OperatingSystem.UseNativeData ? OperatingSystem.NativeVersionString : Environment.OSVersion.VersionString, OperatingSystem.GetSystemVersionString());
+			string version = OperatingSystem.VersionString;
+			_output.WriteLine(version);
+			Assert.NotNull(version);
+			Assert.NotEqual("", version);
 		}
 
 		[Fact]
 		public void CorrectVersionTest()
 		{
-			Assert.Equal(OperatingSystem.UseNativeData ? OperatingSystem.NativeVersion : Environment.OSVersion.Version, OperatingSystem.GetSystemVersion());
+			Version version = OperatingSystem.Version;
+			_output.WriteLine(version.ToString());
+			Assert.NotEqual(new Version(0, 0, 0), version);
 		}
 	}
 }
--- NorthwoodLib/OperatingSystem.cs
+++ NorthwoodLib/OperatingSystem.cs
@@ -1,64 +1,134 @@
 using System;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
-// ReSharper disable HeapView.BoxingAllocation
 
 namespace NorthwoodLib
 {
 	public static class OperatingSystem
 	{
+#pragma warning disable IDE1006
 		[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
 		private struct OSVERSIONINFO
 		{
-			// The OSVersionInfoSize field must be set to Marshal.SizeOf<OSVERSIONINFO>()
-			internal uint OSVersionInfoSize;
-			internal readonly uint MajorVersion;
-			internal readonly uint MinorVersion;
-			internal readonly uint BuildNumber;
-			private readonly uint PlatformId;
+			// The dwOSVersionInfoSize field must be set to Marshal.SizeOf<OSVERSIONINFO>()
+			internal uint dwOSVersionInfoSize;
+			internal readonly uint dwMajorVersion;
+			internal readonly uint dwMinorVersion;
+			internal readonly uint dwBuildNumber;
+			private readonly uint dwPlatformId;
 			[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
-			internal readonly string CSDVersion;
+			internal readonly string szCSDVersion;
+			internal readonly ushort wServicePackMajor;
+			internal readonly ushort wServicePackMinor;
+			private readonly ushort wSuiteMask;
+			internal readonly byte wProductType;
+			private readonly byte wReserved;
 		}
+#pragma warning restore IDE1006
 
-		[DllImport("ntdll.dll")]
+		[DllImport("ntdll")]
 		private static extern uint RtlGetVersion(ref OSVERSIONINFO versionInfo);
 
-		[DllImport("ntdll.dll")]
+		[DllImport("ntdll")]
 		private static extern int RtlNtStatusToDosError(uint status);
 
-		internal static readonly bool UseNativeData;
-		internal static readonly Version NativeVersion;
-		internal static readonly string NativeVersionString;
+		[DllImport("user32")]
+		private static extern int GetSystemMetrics(int id);
+
+		public static readonly bool UsesNativeData;
+		public static readonly Version Version;
+		public static readonly string VersionString;
 
 		static OperatingSystem()
 		{
 			if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
 			{
-				UseNativeData = false;
+				Version = Environment.OSVersion.Version;
+				VersionString = $"{Environment.OSVersion.VersionString} {Environment.OSVersion.ServicePack}".Trim();
+				UsesNativeData = false;
 				return;
 			}
+
 			OSVERSIONINFO osVersionInfo = new OSVERSIONINFO
 			{
-				OSVersionInfoSize = (uint) Marshal.SizeOf<OSVERSIONINFO>()
+				dwOSVersionInfoSize = (uint) Marshal.SizeOf<OSVERSIONINFO>()
 			};
 			uint status = RtlGetVersion(ref osVersionInfo);
 			if (status != 0)
 				throw new Win32Exception(RtlNtStatusToDosError(status));
-			UseNativeData = true;
-			NativeVersion = new Version((int) osVersionInfo.MajorVersion, (int) osVersionInfo.MinorVersion, (int) osVersionInfo.BuildNumber);
-			NativeVersionString = $"Windows {osVersionInfo.MajorVersion}.{osVersionInfo.MinorVersion}.{osVersionInfo.BuildNumber}";
-			if (!string.IsNullOrEmpty(osVersionInfo.CSDVersion))
-				NativeVersionString += " " + osVersionInfo.CSDVersion;
+
+			UsesNativeData = true;
+			Version = new Version((int) osVersionInfo.dwMajorVersion, (int) osVersionInfo.dwMinorVersion, (int) osVersionInfo.dwBuildNumber);
+
+			// ReSharper disable HeapView.BoxingAllocation
+			VersionString = $"Windows {ProcessWindowsVersion(osVersionInfo)} {osVersionInfo.dwMajorVersion}.{osVersionInfo.dwMinorVersion}.{osVersionInfo.dwBuildNumber}";
+			if (osVersionInfo.wServicePackMajor != 0 || osVersionInfo.wServicePackMinor != 0)
+				VersionString += $"-{osVersionInfo.wServicePackMajor}.{osVersionInfo.wServicePackMinor}";
+			if (!string.IsNullOrWhiteSpace(osVersionInfo.szCSDVersion))
+				VersionString += $" {osVersionInfo.szCSDVersion}";
+			VersionString += $" {(Environment.Is64BitOperatingSystem ? "64bit" : "32bit")} Process: {IntPtr.Size * 8}bit";
+			VersionString = VersionString.Trim();
+			// ReSharper restore HeapView.BoxingAllocation
 		}
 
-		public static string GetSystemVersionString()
+		private static string ProcessWindowsVersion(OSVERSIONINFO version)
 		{
-			return UseNativeData ? NativeVersionString : Environment.OSVersion.VersionString;
-		}
+			if (version.wProductType == 1)
+				switch (version.dwMajorVersion)
+				{
+					case 10:
+						switch (version.dwMinorVersion)
+						{
+							case 0: return "10";
+						}
+						break;
+					case 6:
+						switch (version.dwMinorVersion)
+						{
+							case 3: return "8.1";
+							case 2: return "8";
+							case 1: return "7";
+							case 0: return "Vista";
+						}
+						break;
+					case 5:
+						switch (version.dwMinorVersion)
+						{
+							case 2: return "XP Professional x64 Edition";
+							case 1: return "XP";
+							case 0: return "2000";
+						}
+						break;
+				}
+			else
+				switch (version.dwMajorVersion)
+				{
+					case 10:
+						switch (version.dwMinorVersion)
+						{
+							case 0 when version.dwBuildNumber >= 17677: return "Server 2019";
+							case 0: return "Server 2016";
+						}
+						break;
+					case 6:
+						switch (version.dwMinorVersion)
+						{
+							case 3: return "Server 2012 R2";
+							case 2: return "Server 2012";
+							case 1: return "Server 2008 R2";
+							case 0: return "Server 2008";
+						}
+						break;
+					case 5:
+						switch (version.dwMinorVersion)
+						{
+							case 2 when GetSystemMetrics(89) != 0: return "Server 2003 R2";
+							case 2: return "Server 2003";
+						}
+						break;
+				}
 
-		public static Version GetSystemVersion()
-		{
-			return UseNativeData ? NativeVersion : Environment.OSVersion.Version;
+			return "Unknown";
 		}
 	}
 }
