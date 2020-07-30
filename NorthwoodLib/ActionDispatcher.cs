308001ca922a51a7e5785b374999b7fe7f559f0b
petris <paprikapolishgamer@gmail.com>
2020-07-30 17:55:23 +0200

    Add missing documentation

--- NorthwoodLib/ActionDispatcher.cs
+++ NorthwoodLib/ActionDispatcher.cs
@@ -3,15 +3,25 @@ using System.Collections.Concurrent;
 
 namespace NorthwoodLib
 {
+	/// <summary>
+	/// Queues <see cref="Action"/> and runs them on <see cref="Invoke"/>
+	/// </summary>
 	public sealed class ActionDispatcher
 	{
 		private readonly ConcurrentQueue<Action> _actionQueue = new ConcurrentQueue<Action>();
 
+		/// <summary>
+		/// Queues an <see cref="Action"/>
+		/// </summary>
+		/// <param name="action">Queued <see cref="Action"/></param>
 		public void Dispatch(Action action)
 		{
 			_actionQueue.Enqueue(action);
 		}
 
+		/// <summary>
+		/// Runs all scheduled <see cref="Action"/>
+		/// </summary>
 		public void Invoke()
 		{
 			while (_actionQueue.TryDequeue(out Action action))
