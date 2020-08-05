c1d5aafa106c1c4cd06e75263a0eb2bbccdc945b
petris <paprikapolishgamer@gmail.com>
2020-08-05 11:30:58 +0200

    Fix documentation

--- NorthwoodLib/WineInfo.cs
+++ NorthwoodLib/WineInfo.cs
@@ -6,6 +6,9 @@ using System.Text;
 
 namespace NorthwoodLib
 {
+	/// <summary>
+	/// Detects usage of <see href="https://www.winehq.org/">Wine</see> and informs about its version
+	/// </summary>
 	public static unsafe class WineInfo
 	{
 #pragma warning disable IDE1006
@@ -21,28 +24,28 @@ namespace NorthwoodLib
 		private const string Ntdll = "ntdll";
 
 		/// <summary>
-		/// Returns used Wine version https://wiki.winehq.org/Developer_FAQ#How_can_I_detect_Wine.3F
+		/// Returns used Wine version <see href="https://wiki.winehq.org/Developer_FAQ#How_can_I_detect_Wine.3F"/>
 		/// </summary>
 		/// <returns>Used Wine version</returns>
 		[DllImport(Ntdll, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl, EntryPoint = "wine_get_version")]
 		private static extern string GetWineVersion();
 
 		/// <summary>
-		/// Returns used Wine build https://source.winehq.org/git/wine.git/blob/HEAD:/include/wine/library.h
+		/// Returns used Wine build <see href="https://source.winehq.org/git/wine.git/blob/HEAD:/include/wine/library.h"/>
 		/// </summary>
 		/// <returns>Used Wine build</returns>
 		[DllImport(Ntdll, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl, EntryPoint = "wine_get_build_id")]
 		private static extern string GetWineBuildId();
 
 		/// <summary>
-		/// Returns used Wine Staging patches https://github.com/wine-staging/wine-staging/blob/master/patches/Staging/0003-loader-Add-commandline-option-patches-to-show-the-pa.patch
+		/// Returns used Wine Staging patches <see href="https://github.com/wine-staging/wine-staging/blob/master/patches/Staging/0003-loader-Add-commandline-option-patches-to-show-the-pa.patch"/>
 		/// </summary>
 		/// <returns>Used Wine Staging patches</returns>
 		[DllImport(Ntdll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "wine_get_patches")]
 		private static extern WinePatch* GetWinePatches();
 
 		/// <summary>
-		/// Returns Wine host https://source.winehq.org/git/wine.git/blob/HEAD:/include/wine/library.h
+		/// Returns Wine host <see href="https://source.winehq.org/git/wine.git/blob/HEAD:/include/wine/library.h"/>
 		/// </summary>
 		/// <returns>Used Wine host</returns>
 		[DllImport(Ntdll, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl, EntryPoint = "wine_get_host_version")]
